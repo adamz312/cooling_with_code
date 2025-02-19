cf533196a261a5e81dc74f7d498e87de19eb700e
FranciscoLozCoding <francisco.lozano33@outlook.com>
2025-02-19 12:59:24 -0600

    Add global variables for project configuration

---
+++ tools/environment.py
@@ -0,0 +1,7 @@
+'''This file is for defining global variables for the project.'''
+
+# used for the defining the random state in models
+RANDOM_STATE=123
+
+# used for defining the validation size in train_test_split
+VALID_SPLIT=0.3
