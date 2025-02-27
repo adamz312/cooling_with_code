be7264379c661ea1026a2d2c42e7723ac414a0f7
FranciscoLozCoding <francisco.lozano33@outlook.com>
2025-02-26 23:17:21 -0600

    feat: move univariate outlier analysis to a new module and clean up distribution tools

--- tools/distribution.py
+++ tools/distribution.py
@@ -1,7 +1,5 @@
 """Distribution tools for the project."""
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import MinMaxScaler
-import seaborn as sns
 
 def plot_target_var_distribution(orginal_dataset, train_split, valid_split):
     """
@@ -36,26 +34,3 @@ def plot_target_var_distribution(orginal_dataset, train_split, valid_split):
     plt.title('Distribution of UHI: Original, Training, and Validation Splits')
     plt.legend()
     return plt
-
-def univariate_outlier_analysis(df, dataset_name):
-    """
-    This function conducts a univariate outlier analysis on the DataFrame passed in.
-
-    Args:
-        df (pandas.DataFrame): The DataFrame to analyze.
-        dataset_name (str): The name of the dataset.
-
-    Returns:
-        None
-    """
-    # Scale the data using MinMaxScaler
-    scaler = MinMaxScaler()
-    outlier_df = scaler.fit_transform(df)
-
-    # Create the box plot
-    plt.figure(figsize=(20, 10))
-    sns.boxplot(data=outlier_df)
-    plt.xticks(ticks=range(len(df.columns)), labels=df.columns, rotation=45, ha='right') # Rotate x-axis labels for readability
-    plt.tight_layout()
-    plt.title(f'{dataset_name} Univariate Outlier Analysis')
-    plt.show()
